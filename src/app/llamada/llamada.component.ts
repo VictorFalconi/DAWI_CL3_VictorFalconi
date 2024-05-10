33881e31080090f4a4bad0f6b1e75787c588efe2
vfalconi <vfalconi@bluepeople.com>
2024-05-10 12:17:27 -0500

    Llamada resuelto

--- src/app/llamada/llamada.component.ts
+++ src/app/llamada/llamada.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-llamada',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './llamada.component.html',
   styleUrl: './llamada.component.scss'
 })
 export class LlamadaComponent {
-
+  llamada = {
+    resultado: "",
+    minutos: 0,
+  }
+  mostrarMensaje = false;
+  calcularPago() {
+    if(Number(this.llamada.minutos) > 5) {
+      this.llamada.resultado = "El total a pagar es S/" + Number(this.llamada.minutos) * 2;
+    } else {
+      this.llamada.resultado = "El total a pagar es S/10";
+    }
+    this.mostrarMensaje = true;
+  }
 }
