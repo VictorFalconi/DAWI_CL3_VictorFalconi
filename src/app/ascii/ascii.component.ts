e104ab6e5745ffbb5e0a7b0a8cc2689e93c38c3a
vfalconi <vfalconi@bluepeople.com>
2024-05-10 13:04:55 -0500

    Ascii terminado

--- src/app/ascii/ascii.component.ts
+++ src/app/ascii/ascii.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './ascii.component.scss'
 })
 export class AsciiComponent {
-
+  lista: {codigo: number, caracter: string}[] = [];
+  mostrarTabla = false;
+  generarLista() {
+    for(let i = 32; i < 97; i++) {
+      let elemento = {codigo: i, caracter: String.fromCharCode(i)};
+      this.lista.push(elemento);
+    }
+    for(let i = 123; i < 165; i++) {
+      let elemento = {codigo: i, caracter: String.fromCharCode(i)};
+      this.lista.push(elemento);
+    }
+    this.mostrarTabla = true;
+  }
 }
